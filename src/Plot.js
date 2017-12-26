7eadc6b461819c982a4e1c67ae4f7287aebf60bd
Erik Hornberger <hornbergererik@gmail.com>
2017-12-26 07:53:36 -0500

    Add zoom

--- src/Plot.js
+++ src/Plot.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react'
-import { VictoryChart, VictoryArea } from 'victory'
+import {
+  VictoryChart,
+  VictoryArea,
+  VictoryGroup,
+  VictoryZoomContainer,
+  VictoryScatter
+} from 'victory'
 import Websocket from 'react-websocket'
 
 export default class Plot extends Component {
   constructor(props) {
     super(props)
+    this.domain = this.domain.bind(this)
     this.state = {
-      data: [{ x: new Date(), y: 0 }]
+      data: []
     }
   }
 
@@ -14,27 +21,42 @@ export default class Plot extends Component {
     return (
       <VictoryChart
         className="Plot"
+        domain={this.domain()}
+        domainPadding={{ top: 0, left: 0, bottom: 0, right: 0 }}
         padding={{ top: 10, left: 30, bottom: 0, right: 0 }}
+        containerComponent={<VictoryZoomContainer zoomDimension="x" />}
       >
-        <VictoryArea
-          style={{ data: { fill: '#c43a31' } }}
-          data={this.state.data}
-        />
-        <Websocket
-          url="ws://127.0.0.1:8080"
-          onMessage={data => {
-            const json = JSON.parse(data)
-            const cost = json.price
-            const date = new Date(json.date)
-            const sample = { x: date, y: cost }
-            this.setState(prevState => ({
-              data: [...prevState.data, sample]
-            }))
-            console.log('New Data: ' + sample)
-            console.log('Data length: ' + this.state.data.length)
-          }}
-        />
+        <VictoryGroup>
+          <VictoryArea
+            style={{ data: { fill: '#c43a31' } }}
+            data={this.state.data}
+          />
+          <Websocket
+            url="ws://127.0.0.1:8080"
+            onMessage={data => {
+              const json = JSON.parse(data)
+              const samples = json.map(x => ({
+                x: new Date(x.date),
+                y: x.price
+              }))
+              this.setState(prevState => ({
+                data: [...samples, ...prevState.data]
+              }))
+            }}
+          />
+        </VictoryGroup>
       </VictoryChart>
     )
+  }
+
+  domain() {
+    if (this.state.data.length < 2) {
+      return { x: [0, 100], y: [0, 100] }
+    } else {
+      const prices = this.state.data.map(data => data.y)
+      const minPrice = Math.min(...prices)
+      const maxPrice = Math.max(...prices)
+      return { y: [minPrice, maxPrice] }
+    }
   }
 }
