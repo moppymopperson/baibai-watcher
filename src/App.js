6bf4eec1fa00d43006593f7305829966e192c008
Erik Hornberger <hornbergererik@gmail.com>
2018-01-01 09:05:33 -0500

    Removed header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react'
 import Plot from './Plot'
-import logo from './logo.svg'
 import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">ビット売買</h1>
-        </header>
         <div className="Container">
           <Plot />
         </div>
